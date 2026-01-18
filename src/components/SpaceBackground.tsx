57dc9446b0b43c62601696b171eec8d3439e6ce4
PawanTheGod <pawanshivajighule25@gmail.com>
2026-01-18 23:51:34 +0530

    Make stars brighter and more visible for rotation effect

--- src/components/SpaceBackground.tsx
+++ src/components/SpaceBackground.tsx
@@ -3,15 +3,15 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial } from '@react-three/drei';
 import * as random from 'maath/random';
 
-// Minimal stars for ChatGPT aesthetic
+// Bright visible stars
 function Stars(props: any) {
   const ref = useRef<any>();
-  const positions = useMemo(() => random.inSphere(new Float32Array(2000 * 3), { radius: 1.8 }), []);
+  const positions = useMemo(() => random.inSphere(new Float32Array(3000 * 3), { radius: 1.8 }), []);
 
   useFrame((_state, delta) => {
     if (ref.current) {
-      ref.current.rotation.x -= delta / 20;
-      ref.current.rotation.y -= delta / 25;
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 12;
     }
   });
 
@@ -21,10 +21,10 @@ function Stars(props: any) {
         <PointMaterial
           transparent
           color="#ffffff"
-          size={0.002}
+          size={0.004}
           sizeAttenuation={true}
           depthWrite={false}
-          opacity={0.15}
+          opacity={0.6}
         />
       </Points>
     </group>
@@ -35,7 +35,7 @@ export default function SpaceBackground() {
   return (
     <div className="fixed inset-0 z-0 bg-black" style={{ pointerEvents: 'none' }}>
       <Canvas camera={{ position: [0, 0, 1], fov: 75 }}>
-        <ambientLight intensity={0.1} />
+        <ambientLight intensity={0.2} />
         <Stars />
       </Canvas>
     </div>
